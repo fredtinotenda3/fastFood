01de0e44701b337ab21b510375e20666e3ec0a22
Tinotenda Fred <fredtinotenda3@gmail.com>
2025-07-06 13:36:17 +0200

    Implement: Stlyle, Initialiize Custom Styles within tailwind, Import Fonts, Icons and Images

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+    <View className="flex-1 items-center justify-center bg-white-100">
+      <Text className="text-5xl text-center  font-bold text-primary font-quicksand-bold">
+        Welcome to my React Native App!
       </Text>
     </View>
   );
